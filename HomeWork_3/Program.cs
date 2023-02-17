0d037530eef892421e01e64804ef729a63d93818
Marina Barinova <marina.alive888@yandex.ru>
2023-02-17 23:05:08 +0300

    Задача 3

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 29. Напишите программу, которая задает массив из произвольного 
+//количества элементов и выводит их на экран
+//1,2,5,7,19,->[1,2,5,7,19]
+//6,1,33 ->[6,1,33]
+
+
+
+int [] CreatNewArray(int size_array)
+{
+    int[] random01array = new int [size_array];
+    for (int i = 0; i < size_array; i++)
+    {
+        random01array[i] = new Random().Next(0,1000);
+    }
+    return  random01array;
+}
+void ShowCreatArray(int[] array01)
+{
+    Console.WriteLine("Your array is ");
+    for (int i = 0; i < array01.Length; i++)
+    Console.Write(array01[i] + " " );
+}
+int[]arrayNew = CreatNewArray(15);
+ShowCreatArray(arrayNew);
