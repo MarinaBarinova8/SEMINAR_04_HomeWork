8755cf6f068d4d8803b64ba180262ce5026d103b
Marina Barinova <marina.alive888@yandex.ru>
2023-02-18 13:17:53 +0300

    Задача 2

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -3,39 +3,7 @@
 //452->11; 82->10; 9012->12
 
 
-/*void SummLen(int num)
-{
-    int count = 1;
-    while(num>=10)
-    {
-        num =num/10;
-        count++;
-    }
 
-
-    int sum = 0;
-    for (int i = 1; i <= count ; i++)
-    {
-        num= num / 10;
-        sum = sum + num % 10;
-        
-    }
-    Console.Write($"{num}-> {sum}");
-}
-     
-Console.Write("Input your number: ");
-int user_num = Convert.ToInt32(Console.ReadLine());
-
-if (user_num > 0)
-{
-    SummLen(user_num);
-    
-}
-
-else 
-    Console.WriteLine("Impossible value!");
-
-*/
 
 
 void Sumnum(int num)
